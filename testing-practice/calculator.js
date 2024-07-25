9fb2310ed2b043c44072a1709740c4d6cf67da2f
tomeklarysz <ttomek.larysz@gmail.com>
2024-07-25 23:26:46 +0200

    calculator object with methods

---
+++ testing-practice/calculator.js
@@ -0,0 +1,14 @@
+export const Calculator = {
+  add(num1, num2) {
+    return num1 + num2
+  },
+  subtract(num1, num2) {
+    return num1 - num2
+  },
+  divide(num1, num2) {
+    return num1 / num2
+  },
+  multiply(num1, num2) {
+    return num1 * num2
+  },
+}
